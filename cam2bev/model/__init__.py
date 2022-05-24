52d39c2f64c8277cdc2f18e8704648219e545b23
leonsht <leonsht@yandex-team.ru>
2022-05-25 00:31:43 +0300

    introduce v2 training pt2

--- cam2bev/model/__init__.py
+++ cam2bev/model/__init__.py
@@ -1,5 +1,6 @@
 from . import types
 from . import v1
+from . import v2
 
 import omegaconf
 
@@ -7,5 +8,8 @@ import omegaconf
 def get_model(config: omegaconf.DictConfig) -> types.I2IModel:
     if config.type == 'NUNetV1':
         return v1.NUNetV1(**config.params)
+
+    if config.type == 'NUNetV2':
+        return v2.NUNetV2(**config.params)
 
     raise NotImplementedError
